753fedde31ef2eeefeb12dda52d5b016334fbc1d
MaxDM <maxdiazm.2004@gmail.com>
2026-03-25 18:52:57 -0600

    1st commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Usar una imagen oficial de Node.js ligera
+FROM node:18-alpine
+
+# Crear el directorio de trabajo dentro del contenedor
+WORKDIR /app
+
+# Copiar primero los archivos de dependencias (para optimizar la caché de Docker)
+COPY package*.json ./
+
+# Instalar las dependencias
+RUN npm install
+
+# Copiar el resto del código de tu proyecto
+COPY . .
+
+# Exponer el puerto en el que corre tu app
+EXPOSE 8001
+
+# Comando para iniciar la aplicación en producción
+CMD ["npm", "start"]
---
+++ src/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // Para poder recibir transacciones JSON en POST requests [cite: 20]
+
+const PORT = process.env.PORT || 8001; // Puerto 8001 para el Nodo 1
+
+// Endpoint para compartir la cadena
+app.get("/express", (req, res) => {
+  res.json({
+    mensaje: "Blockchain local del Nodo Express",
+    longitud: 0,
+    cadena: [],
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Nodo Express corriendo en http://localhost:${PORT}`);
+});
